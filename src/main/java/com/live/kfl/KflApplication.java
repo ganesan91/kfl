784b6143f851554e19789257a0ba7458b3731fff
ganesan91 <ganesantn.kongu@gmail.com>
2018-12-28 13:16:21 +0530

    Inital_Commit

---
+++ src/main/java/com/live/kfl/KflApplication.java
@@ -0,0 +1,14 @@
+package com.live.kfl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KflApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KflApplication.class, args);
+	}
+
+}
+

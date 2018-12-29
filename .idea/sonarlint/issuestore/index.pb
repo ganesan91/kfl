62b809e2a3e5d797acabab52c9849eb7925ed99e
pk <pkarthick35@gmail.com>
2018-12-29 15:15:16 +0530

    Added checkstyle, sonarLint, Log4j

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+S
+#src/main/resources/log4j.properties,7\5\755950bb690295904a49d36e60958901c54013b8
+Y
+)src/main/resources/application.properties,0\6\06f65155ec73600b9fc5c4dc2883d4bb84d8e284
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+j
+:src/main/java/com/live/kfl/application/KflApplication.java,5\6\56a72c0825a5ec6682774dfc1e4d0dfda3e43dde
--- src/main/java/com/live/kfl/application/KflApplication.java
+++ src/main/java/com/live/kfl/application/KflApplication.java
@@ -1,14 +1,16 @@
 package com.live.kfl.application;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KflApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KflApplication.class, args);
-	}
+  private static final Logger logger = Logger.getLogger(KflApplication.class);
 
+  public static void main(String[] args) {
+    SpringApplication.run(KflApplication.class, args);
+    logger.info("Application Started");
+  }
 }
-
